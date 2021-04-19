1e30f70ad7b45976cdf2462c8aefee54592cb328
mitrotasios <a.mitrotasios13@gmail.com>
2021-04-19 22:42:33 +0100

    change password admin

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,7 +1,8 @@
 import React, { createRef, Component } from 'react';
 import AddUser from './AddUser'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignOutAlt, faPlus, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faSignOutAlt, faPlus, faSearch, faTrash } from '@fortawesome/free-solid-svg-icons'
+import ResetPassword from './ResetPassword';
 
 class Admin extends Component {
     constructor(props) {
@@ -10,7 +11,9 @@ class Admin extends Component {
         this.state = {
             users: this.props.users,
             usersRenderList: this.props.users,
-            isModalOpen: false 
+            isAddUserModalOpen: false,
+            isPasswordModalOpen: false,  
+            selectedUser: null
         }
     }
 
@@ -46,19 +49,49 @@ class Admin extends Component {
         this.props.putUser(updatedUser);
     }
 
-    handleModalShow = () => {
+    handleAddUserModalShow = () => {
         this.setState({
-            isModalOpen: true
+            isAddUserModalOpen: true
         });
     }
 
-    handleModalClose = () => {
+    handleAddUserModalClose = () => {
         this.setState({
-            isModalOpen: false
+            isAddUserModalOpen: false
+        });
+    }
+
+    handlePasswordModalShow = (user) => {
+        this.setState({
+            isPasswordModalOpen: true,
+            selectedUser: user
+        });
+    }
+
+    handlePasswordModalClose = () => {
+        this.setState({
+            isPasswordModalOpen: false
         });
     }
 
     render() {
+        if (this.props.users.isLoading) {
+            <div className="row flex-fill h-100 d-flex">
+                <div className="col">
+                    <div style={{"z-index":"10", "position":"fixed", "border-bottom":"1px solid #E2E2E4", "background-color": "white", "margin-top": "0px", "padding": "10px", "width":"110%"}} className="row header">
+                        <div className="col ml-2">
+                            <a href="/" onClick={this.props.logoutUser}>
+                            <FontAwesomeIcon style={{"verticalAlign":"middle"}} icon={faSignOutAlt} size='lg' rotation={180}/>
+                            <span style={{"verticalAlign":"middle"}} className="menu-header"> Sign Out </span>
+                            </a>
+                        </div>
+                    </div>
+                    <div style={{"border-right":"1px solid #E2E2E4", "paddingTop":"81px", "paddingLeft":"10px"}} className="row">
+                        Loading
+                    </div>
+                </div>
+            </div>
+        }
         return (
             <div id="page-wrap" className="container-fluid">         
                 <div className="row flex-fill h-100 d-flex">
@@ -66,7 +99,7 @@ class Admin extends Component {
                         <div style={{"z-index":"10", "position":"fixed", "border-bottom":"1px solid #E2E2E4", "background-color": "white", "margin-top": "0px", "padding": "10px", "width":"110%"}} className="row header">
                             <div className="col ml-2">
                                 <a href="/" onClick={this.props.logoutUser}>
-                                <FontAwesomeIcon style={{"verticalAlign":"middle"}} icon={faSignOutAlt} size='lg'/>
+                                <FontAwesomeIcon style={{"verticalAlign":"middle"}} icon={faSignOutAlt} size='lg' rotation={180}/>
                                 <span style={{"verticalAlign":"middle"}} className="menu-header"> Sign Out </span>
                                 </a>
                             </div>
@@ -75,64 +108,66 @@ class Admin extends Component {
                             <div className="col-6 ml-2">
                                 <div className="container-fluid">
                                     <div className="row">
-                                        <span style={{"verticalAlign":"middle"}}>User Management</span>
-                                        <span><FontAwesomeIcon style={{"verticalAlign":"middle", "marginLeft":"30px"}} icon={faSearch} size='sm'/><input style={{"border":"1px solid lightgrey", "borderRadius":"4px", "marginLeft":"5px"}} type="text" onChange={(e => this.handleChange(e))}/></span>
-                                        <span className="ml-auto"><a style={{"verticalAlign":"middle"}} type="button" onClick={this.handleModalShow}><FontAwesomeIcon icon={faPlus} size='sm'/>{' '}Add New User</a></span>
+                                        <span style={{"color":"#432F87", "fontSize":"20px"}}>User Management</span>
+                                        <span><FontAwesomeIcon style={{"marginLeft":"30px"}} icon={faSearch} size='sm'/><input style={{"border":"1px solid lightgrey", "borderRadius":"4px", "marginLeft":"5px"}} type="text" onChange={(e => this.handleChange(e))}/></span>
+                                        <span className="ml-auto"><a style={{"color":"#432F87"}} type="button" onClick={this.handleAddUserModalShow}><FontAwesomeIcon icon={faPlus} size='sm'/>{' '}Add New User</a></span>
                                     </div>
                                     <div className="row">
                                         <ul style={{"width":"100%"}} className="list-unstyled">
                                             {this.state.usersRenderList.map(user => {
-                                                return(
-                                                    <li key={user._id}>
-                                                        {
-                                                            user.status!="DELETED" ? (
-                                                                <div style={{"border":"1px solid lightgrey", "padding":"20px", "borderRadius":"7px", "backgroundColor":"white", "boxShadow":"0px 0px 3px 0px lightgrey"}} key={user._id} className="user-card">
-                                                                    <span>{user.username}</span>
-                                                                    <span style={{"float":"right"}}>
-                                                                        {
-                                                                            user.admin ? (
-                                                                                <span>Admin</span>
-                                                                            ) : (
-                                                                                <select style={{"borderRadius":"3px", "height":"27px"}} onChange={(e) => this.changeUserRole(user, e)}>
-                                                                                    <option selected={user.supervisor ? ("selected") : ""}>Supervisor User</option>
-                                                                                    <option selected={!user.supervisor ? ("selected") : ""}>Regular User</option>
-                                                                                </select>
-                                                                            )
-                                                                        }
-                                                                    </span>
-                                                                    <span style={{"float":"right", "height":"27px","border":"1px solid #432F87", "backgroundColor":"#432F87", "color":"white", "borderRadius":"3px", "padding":"0px 3px 0px 3px", "margin-left":"10px", "margin-right":"10px"}}>
-                                                                        <a type="button">Reset Password</a>
-                                                                    </span>
-                                                                    <span style={{"float":"right", "height":"27px","border":"1px solid #432F87", "borderRadius":"3px", "color":"#432F87", "padding":"0px 3px 0px 3px", "margin-left":"10px", "margin-right":"10px"}}>
-                                                                        <a onClick={() => this.deleteUser(user)} type="button">Delete User</a>
-                                                                    </span>
-                                                                </div>
-                                                            ) : (
-                                                                <div style={{"border":"1px solid lightgrey", "padding":"20px", "borderRadius":"7px", "backgroundColor":"#E2E2E4", "boxShadow":"0px 0px 3px 0px lightgrey"}} key={user._id} className="user-card">
-                                                                    <span>{user.username}</span>
-                                                                    <span style={{"float":"right"}}>
-                                                                        {
-                                                                            user.admin ? (
-                                                                                <span>Admin</span>
-                                                                            ) : (
-                                                                                <select style={{"borderRadius":"3px", "height":"27px", "backgroundColor":"grey", "color":"lightgrey"}} onChange={(e) => this.changeUserRole(user, e)}>
-                                                                                    <option selected={user.supervisor ? ("selected") : ""}>Supervisor User</option>
-                                                                                    <option selected={!user.supervisor ? ("selected") : ""}>Regular User</option>
-                                                                                </select>
-                                                                            )
-                                                                        }
-                                                                    </span>
-                                                                    <span style={{"float":"right", "height":"27px","border":"1px solid grey", "backgroundColor":"grey", "color":"lightgrey", "borderRadius":"3px", "padding":"0px 3px 0px 3px", "margin-left":"10px", "margin-right":"10px"}}>
-                                                                        <a type="button">Reset Password</a>
-                                                                    </span>
-                                                                    <span style={{"float":"right", "height":"27px","border":"1px solid #432F87", "borderRadius":"3px", "backgroundColor":"#432F87", "color":"white", "padding":"0px 3px 0px 3px", "margin-left":"10px", "margin-right":"10px"}}>
-                                                                        <a onClick={() => this.reActivateUser(user)} type="button">Reactivate</a>
-                                                                    </span>
-                                                                </div>
-                                                            )
-                                                        }
-                                                    </li>
-                                                );
+                                                if (user.username != JSON.parse(localStorage.getItem('userData')).username) {
+                                                    return(
+                                                        <li key={user._id}>
+                                                            {
+                                                                user.status!="DELETED" ? (
+                                                                    <div style={{"border":"1px solid lightgrey", "padding":"20px", "borderRadius":"7px", "backgroundColor":"white", "boxShadow":"0px 0px 3px 0px lightgrey"}} key={user._id} className="user-card">
+                                                                        <span>{user.username}</span>
+                                                                        <span style={{"float":"right"}}>
+                                                                            {
+                                                                                user.admin ? (
+                                                                                    <span>Admin</span>
+                                                                                ) : (
+                                                                                    <select style={{"borderRadius":"3px", "height":"27px"}} onChange={(e) => this.changeUserRole(user, e)}>
+                                                                                        <option selected={user.supervisor ? ("selected") : ""}>Supervisor User</option>
+                                                                                        <option selected={!user.supervisor ? ("selected") : ""}>Regular User</option>
+                                                                                    </select>
+                                                                                )
+                                                                            }
+                                                                        </span>
+                                                                        <span style={{"float":"right", "height":"27px","border":"1px solid #432F87", "backgroundColor":"#432F87", "color":"white", "borderRadius":"3px", "padding":"0px 5px 0px 5px", "margin-left":"10px", "margin-right":"10px"}}>
+                                                                            <a onClick={() => this.handlePasswordModalShow(user)} type="button">Reset Password</a>
+                                                                        </span>
+                                                                        <span style={{"float":"right", "height":"27px", "borderRadius":"3px", "color":"#432F87", "padding":"0px 3px 0px 3px", "margin-left":"10px", "margin-right":"10px"}}>
+                                                                            <a onClick={() => this.deleteUser(user)} type="button"><FontAwesomeIcon style={{"verticalAlign":"middle"}} icon={faTrash} size='lg'/></a>
+                                                                        </span>
+                                                                    </div>
+                                                                ) : (
+                                                                    <div style={{"border":"1px solid lightgrey", "padding":"20px", "borderRadius":"7px", "backgroundColor":"#E2E2E4", "boxShadow":"0px 0px 3px 0px lightgrey"}} key={user._id} className="user-card">
+                                                                        <span>{user.username}</span>
+                                                                        <span style={{"float":"right"}}>
+                                                                            {
+                                                                                user.admin ? (
+                                                                                    <span>Admin</span>
+                                                                                ) : (
+                                                                                    <select style={{"borderRadius":"3px", "height":"27px", "backgroundColor":"grey", "color":"lightgrey"}} onChange={(e) => this.changeUserRole(user, e)}>
+                                                                                        <option selected={user.supervisor ? ("selected") : ""}>Supervisor User</option>
+                                                                                        <option selected={!user.supervisor ? ("selected") : ""}>Regular User</option>
+                                                                                    </select>
+                                                                                )
+                                                                            }
+                                                                        </span>
+                                                                        <span style={{"float":"right", "height":"27px","border":"1px solid grey", "backgroundColor":"grey", "color":"lightgrey", "borderRadius":"3px", "padding":"0px 3px 0px 3px", "margin-left":"10px", "margin-right":"10px"}}>
+                                                                            <a type="button">Reset Password</a>
+                                                                        </span>
+                                                                        <span style={{"float":"right", "height":"27px","border":"1px solid #432F87", "borderRadius":"3px", "backgroundColor":"#432F87", "color":"white", "padding":"0px 3px 0px 3px", "margin-left":"10px", "margin-right":"10px"}}>
+                                                                            <a onClick={() => this.reActivateUser(user)} type="button">Reactivate</a>
+                                                                        </span>
+                                                                    </div>
+                                                                )
+                                                            }
+                                                        </li>
+                                                    );
+                                                }
                                             })}
                                         </ul>
                                     </div>
@@ -141,8 +176,10 @@ class Admin extends Component {
                         </div>
                     </div>
                 </div>
-                <AddUser isModalOpen={this.state.isModalOpen} handleModalClose={this.handleModalClose} handleModalOpen={this.handleModalShow} 
+                <AddUser isModalOpen={this.state.isAddUserModalOpen} handleModalClose={this.handleAddUserModalClose} handleModalShow={this.handleAddUserModalShow} 
                     postUser={this.props.postUser} />
+                <ResetPassword isModalOpen={this.state.isPasswordModalOpen} handleModalClose={this.handlePasswordModalClose} handleModalOpen={this.handlePasswordModalShow} 
+                    putUser={this.props.putUser} selectedUser={this.state.selectedUser} />
             </div>
         );
     }
---
+++ src/components/ResetPassword.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import { Modal, Button}  from "react-bootstrap";
+import { Form, Field } from 'react-final-form';
+
+const required = value => (value ? undefined : 'Required')
+const minLength = value => (value.length >= 8 ? undefined : 'Password must be at least 8 characters')
+const containsNumber = value => (/\d/.test(value) ? undefined : 'Password must contain at least one number')
+const matchesOldPassword = oldPassword => value => ( value == oldPassword ? undefined : 'Does not match old password' )
+const newPasswordsMatch = newPassword => value => ( value == newPassword ? undefined : 'Passwords do not match' )
+const composeValidators = (...validators) => value =>
+  validators.reduce((error, validator) => error || validator(value), undefined)
+
+
+class ResetPassword extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleSubmit = (values) => {
+        this.props.putUser({
+            _id: this.props.selectedUser._id,
+            password: values.confirmPassword
+        }, true);
+
+        this.props.handleModalClose();
+
+        alert("Password Successfully Changed!")
+    }
+
+
+    render() {
+        return(
+            <Modal show={this.props.isModalOpen} onHide={this.props.handleModalClose}>
+                <Modal.Header closeButton>
+                    <h4>{this.props.selectedUser ? this.props.selectedUser._id : "HI"}</h4>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="col-12">
+                        <Form
+                            onSubmit={this.handleSubmit}
+                            render={({ handleSubmit, form, submitting, pristine, values }) => (
+                                <div className="container-fluid">
+                                    <div className="row ml-2 mt-2">
+                                        <div className="container-fluid">
+                                            <form id="editReagentForm" onSubmit={handleSubmit}>
+                                                <div className="row">
+                                                    <Field
+                                                    name="newPassword"
+                                                    component="input"
+                                                    type="password"
+                                                    validate={composeValidators(required, minLength, containsNumber)}
+                                                    >
+                                                    {({ input, meta }) => (
+                                                        <div className="col-12">
+                                                            <label>New Password</label>
+                                                            <input {...input} placeholder="New Password"/>
+                                                            {meta.error && meta.submitFailed && <span>{meta.error}</span>}
+                                                        </div>
+                                                    )}
+                                                    </Field>
+                                                </div>
+                                                <div className="row">
+                                                    <Field
+                                                    name="confirmPassword"
+                                                    component="input"
+                                                    type="password"
+                                                    validate={composeValidators(required, newPasswordsMatch(values.newPassword))}
+                                                    >
+                                                    {({ input, meta }) => (
+                                                        <div className="col-12">
+                                                            <label>Confirm Password</label>
+                                                            <input {...input} placeholder="Confirm Password"/>
+                                                            {meta.error && meta.submitFailed && <span>{meta.error}</span>}
+                                                        </div>
+                                                    )}
+                                                    </Field>
+                                                </div>                   
+                                                <div className="row">
+                                                    <div className="col-12">
+                                                        <button type="button"
+                                                            onClick={() => {
+                                                                this.props.handleModalClose();
+                                                                var fields = form.getRegisteredFields()
+                                                                fields.map(field => form.resetFieldState(field))}}>
+                                                            Cancel
+                                                        </button>
+                                                        <button type="submit" disabled={submitting || pristine}>
+                                                            Change Password
+                                                        </button>
+                                                    </div>                                                    
+                                                </div>
+                                            </form>
+                                        </div>                                               
+                                    </div>
+                                </div>
+                            )}
+                        />
+                    </div>
+                </Modal.Body>
+            </Modal>
+        );
+    }
+
+}
+
+export default ResetPassword;
